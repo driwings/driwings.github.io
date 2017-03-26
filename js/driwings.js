881b0fde31c7b9a57dfd63d66e85734587ad6a9b
adrnma <ama0590@gmail.com>
2017-03-25 22:44:14 -0300

    Improve style and labels and include somes patterns

--- js/driwings.js
+++ js/driwings.js
@@ -1,14 +1,15 @@
 $("#modalImg").on("show.bs.modal", (ev) => {
     var source = $(ev.relatedTarget);
     var title = source.find("img").attr("alt");
-    var img = source.find("img").attr("src");
+    var img = "img/" + source.find("img").data("img") + "_min.png";
+    var img_full = "img/" + source.find("img").data("img") + ".png";
     var desc = source.find(".img-desc").html();
 
     var modal = $("#modalImg");
     modal.find(".modal-title").html(title);
     modal.find(".img").attr("src", img);
     modal.find(".img-desc").html(desc);
-    modal.find(".img-download").attr("href", img);
+    modal.find(".img-download").attr("href", img_full);
 });
 
 var em = [68, 72, 73, 83, 70, 68, 83, 9, 67, 85, 78, 80, 78, 73, 64, 84, 103, 64, 74, 70, 78, 75, 9, 68, 72, 74];
